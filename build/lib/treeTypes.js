245eebcc16585668578f786035cfb2e73725dfa0
NewXtSI <leerzeichen32@googlemail.com>
2026-07-05 16:55:38 +0200

    feat: sync grundstueck tree to ioBroker folder objects on adapter start

---
+++ build/lib/treeTypes.js
@@ -0,0 +1,7 @@
+"use strict";
+/**
+ * Shared tree type definitions for the adapter backend.
+ * Mirror of src-admin/src/types/treeTypes.ts (without UI imports).
+ */
+Object.defineProperty(exports, "__esModule", { value: true });
+//# sourceMappingURL=treeTypes.js.map
